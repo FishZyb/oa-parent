2815ad7cb1dc6b25d3bf170b258e055bb0578645
FishZyb <1605727686@qq.com>
2023-03-08 16:09:40 +0800

    完成基本Controller接口的书写

--- service-oa/src/main/java/com/atguigu/auth/ServiceAuthApplication.java
+++ service-oa/src/main/java/com/atguigu/auth/ServiceAuthApplication.java
@@ -2,12 +2,14 @@ package com.atguigu.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author 一只鱼zzz
  * @version 1.0
  */
 @SpringBootApplication
+@ComponentScan("com.atguigu")
 public class ServiceAuthApplication {
   public static void main(String[] args) {
     SpringApplication.run(ServiceAuthApplication.class,args);
--- service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
@@ -4,12 +4,16 @@ import com.atguigu.auth.service.SysRoleService;
 import com.atguigu.auth.service.impl.SysRoleServiceImpl;
 import com.atguigu.common.result.Result;
 import com.atguigu.model.system.SysRole;
+import com.atguigu.vo.system.SysRoleQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.api.R;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,6 +38,79 @@ public class SysRoleController {
     //调用service中的方法进行实现
     List<SysRole> list = sysRoleService.list();
     return Result.ok(list);
+  }
+
+  //条件分页查询
+  //page:当前页 limit：每页显示的记录数
+  //sysRoleQueryVo 条件的对象
+  @ApiOperation("条件分页查询")
+  @GetMapping("{page}/{limit}")
+  public Result pageQueryRole(@PathVariable Long page,
+                              @PathVariable Long limit, SysRoleQueryVo sysRoleQueryVo){
+    //调用service中的方法进行实现
+    //1、创建Page对象，传递分页相关参数
+    Page<SysRole> pageParam = new Page<>(page,limit);
+    //2、封装条件，判断条件是否为空，不为空进行封装
+    LambdaQueryWrapper<SysRole> queryWrapper = new LambdaQueryWrapper<>();
+    String roleName = sysRoleQueryVo.getRoleName();
+    if(!StringUtils.isEmpty(roleName)){
+      //封装
+      queryWrapper.like(SysRole::getRoleName,roleName);
+    }
+    //3、调用方法实现
+    IPage<SysRole> rolePage = sysRoleService.page(pageParam, queryWrapper);
+    return Result.ok(rolePage);
+  }
+
+  @ApiOperation("添加角色")
+  @PostMapping("save")
+  public Result save(@RequestBody SysRole role){
+    //调用service方法
+    boolean save = sysRoleService.save(role);
+    if(save){
+      return Result.ok();
+    }else{
+      return Result.fail();
+    }
+  }
+
+  //根据id查询
+  @ApiOperation("根据id查询")
+  @GetMapping("get/{id}")
+  public Result get(@PathVariable Long id){
+    SysRole sysRole = sysRoleService.getById(id);
+    return Result.ok(sysRole);
+  }
+
+  //修改角色
+  @ApiOperation("修改角色")
+  @PutMapping("update")
+  public Result update(@RequestBody SysRole role){
+    boolean updateById = sysRoleService.updateById(role);
+    if(updateById){
+      return Result.ok();
+    }else{
+      return Result.fail();
+    }
+  }
+
+  //根据id删除角色
+  @ApiOperation("删除角色")
+  @DeleteMapping("remove/{id}")
+  public Result delete(@PathVariable Long id){
+    boolean remove = sysRoleService.removeById(id);
+    if(remove){
+      return Result.ok();
+    }else{
+      return Result.fail();
+    }
+  }
+
+  @ApiOperation(value = "根据id列表删除")
+  @DeleteMapping("batchRemove")
+  public Result batchRemove(@RequestBody List<Long> idList) {
+    sysRoleService.removeByIds(idList);
+    return Result.ok();
   }
 
 }
